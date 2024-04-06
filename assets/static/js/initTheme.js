b86b6862c37f71ead547fc4af1c6b79563eb0e86
Davi Freitas <davimello14@gmail.com>
2024-04-06 13:28:35 -0300

    Added default mapping for apply layout

--- assets/static/js/initTheme.js
+++ assets/static/js/initTheme.js
@@ -2,4 +2,4 @@ const body = document.body;
 const theme = localStorage.getItem('theme')
 
 if (theme) 
-  document.documentElement.setAttribute('data-bs-theme', theme)
+  document.documentElement.setAttribute('data-bs-theme', false)
